6f5347673e8df2b060a979fa5a8209a848b5db13
Redmomn <109732988+Redmomn@users.noreply.github.com>
2023-12-14 15:54:58 +0800

    fix: 加上debug

--- modules/base/v.go
+++ modules/base/v.go
@@ -1,3 +1,15 @@
 package base
 
+import "runtime/debug"
+
 var Version = "unknown"
+
+func init() {
+	if Version != "unknown" {
+		return
+	}
+	info, ok := debug.ReadBuildInfo()
+	if ok {
+		Version = info.Main.Version
+	}
+}
